1f3caf6b502a85708a047835ea01e9ac59941344
werbowym-sheridan <werbowym@sheridancollege.ca>
2024-02-11 15:52:20 -0500

    project initialized to git

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly List<Movie> movies = new List<Movie>
+        {
+            new Movie { Title = "Inception", ReleaseYear = "2010", Duration = "148 min", Rating = 8.8 },
+            new Movie { Title = "The Matrix", ReleaseYear = "1999", Duration = "136 min", Rating = 8.7 },
+            new Movie { Title = "Interstellar", ReleaseYear = "2014", Duration = "169 min", Rating = 8.6 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return movies;
+        }
+
+        // Add other necessary API methods here (POST, PUT, DELETE, etc.)
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Title { get; set; }
+        public required string ReleaseYear { get; set; }
+        public required string Duration { get; set; }
+        public double Rating { get; set; }
+    }
+}
